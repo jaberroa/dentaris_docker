868a100c138a604c39909fe3591f393809edc798
Juan Berroa <jaberroa@hotmail.com>
2026-09-01 23:05:14 -0400

    fix: registra policies de domini

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Inventory;
+use App\Models\Invoice;
+use App\Policies\InventoryPolicy;
+use App\Policies\InvoicePolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Invoice::class, InvoicePolicy::class);
+        Gate::policy(Inventory::class, InventoryPolicy::class);
     }
 }
